c62b386fc308f4f8d9475627c0855c6e75808b7d
Silvia <sylwia.barresi@gmail.com>
2020-04-23 23:24:52 +0200

    milestone 3

--- main.js
+++ main.js
@@ -1,122 +1,145 @@
 
 $(document).ready(function(){
 
+  var pages = $(".movie");
+  var source = document.getElementById("movie-template").innerHTML;
+  var template = Handlebars.compile(source);
+
   $('button').click(function(){
 
-    var pages = $(".movie");
+
     pages.html('');
 
     var movie = $("#search");
     var movieVal = movie.val();
 
-    $.ajax({
-        url: "https://api.themoviedb.org/3/search/movie",
-        method: "GET",
-        dataType: "json",
-        data: {
-            api_key:"3f6e955ea818a96bae170b72ce85c24d",
-            language: "it-IT",
-            query: movieVal
+      $.ajax({
+          url: "https://api.themoviedb.org/3/search/movie",
+          method: "GET",
+          data: {
+              api_key:"3f6e955ea818a96bae170b72ce85c24d",
+              language: "it-IT",
+              query: movieVal
+            },
+          success: function(data){
+            console.log(data);
+            var movie = data.results;
+
+            newOutput(movie, "Film")
           },
-        success: function(data){
-          console.log(data);
-
-          var source = document.getElementById("movie-template").innerHTML;
-          var template = Handlebars.compile(source);
-
-          var movie = data.results;
-
-          for (var i = 0; i<movie.length; i++ ){
-
-            var movieNow = movie[i]
-            context = {
-              title: "Titolo: " + movieNow.title,
-              original_title: "Titolo originale: " + movieNow.original_title,
-              original_language: "Lingua: " + flag(movieNow.original_language),
-              vote_average: "Media voto: " + star(movieNow.vote_average)
-            };
-
-            var html = template(context);
-            $("body").append(html);
+          error: function(error){
+            console.log("Error");
           }
 
-        },
-        error: function(error){
-          console.log("Error");
-        }
+      });
+
+      $.ajax({
+          url: "https://api.themoviedb.org/3/search/tv",
+          method: "GET",
+          data: {
+              api_key:"3f6e955ea818a96bae170b72ce85c24d",
+              language: "it-IT",
+              query: movieVal
+            },
+          success: function(data){
+            console.log(data);
+
+            var series = data.results;
+            newOutput(series, "Serie")
+
+          },
+          error: function(error){
+            console.log("Error");
+          }
+
+      });
 
     });
 
-    $.ajax({
-        url: "https://api.themoviedb.org/3/search/tv",
-        method: "GET",
-        dataType: "json",
-        data: {
-            api_key:"3f6e955ea818a96bae170b72ce85c24d",
-            language: "it-IT",
-            query: movieVal
-          },
-        success: function(data){
-          console.log(data);
+    // funzioni-----------------------------------------------------------------
 
-          var source = document.getElementById("movie-template").innerHTML;
-          var template = Handlebars.compile(source);
+    function newOutput(objects, type){
+      for(var i = 0; i < objects.length; i++){
 
-          var series = data.results;
+        var watchNow = objects[i];
+        var title, original_title;
 
-          for (var i = 0; i<series.length; i++ ){
+        if (type === "Film"){
 
-            var seriesNow = series[i]
-            context = {
-              title: "Titolo: " + seriesNow.name,
-              original_title: "Titolo originale: " + seriesNow.original_name,
-              original_language: "Lingua: " + flag(seriesNow.original_language),
-              vote_average: "Media voto: " + star(seriesNow.vote_average)
-            };
+          title = watchNow.title;
+          original_title = watchNow.original_title;
 
-            var html = template(context);
-            $("body").append(html);
+        }else if (type === "Serie"){
 
-          }
-
-        },
-        error: function(error){
-          console.log("Error");
-        }
-
-    });
-
-          function flag(original_language) {
-
-          var lingue = ['es', 'it', 'en'];
-          var flags;
-
-          if (lingue.includes(original_language)) {
-
-            flags = '<img src=" '+ original_language +' .png">';
-            return flags;
-
-          } else {
-            return original_language;
-          }
+          title = watchNow.name;
+          original_title = watchNow.original_name;
         };
 
 
-        function star(vote_average){
-          var vote = Math.floor(vote_average / 2);
-          console.log(vote);
-          var stars = "";
-          for (var s = 0; s <5; s++) {
-            if (s <= vote){
-              stars += '<i class="fas fa-star"></i>';
-           }else{
-              stars += '<i class="far fa-star"></i>';
-          }
-        }
-        return stars;
+        context = {
+          title: "Titolo: " + title,
+          original_title: "Titolo originale: " + original_title,
+          original_language: "Lingua: " + flag(watchNow.original_language),
+          vote_average: "Media voto: " + star(watchNow.vote_average),
+          poster: posters(watchNow.poster_path),
+          ms: type
+        };
+
+        var html = template(context);
+        $("body").append(html);
+
       };
+    };
 
 
-  });
+    function flag(original_language) {
+
+      var lingue = ['es', 'it', 'en'];
+      var flags;
+
+      if (lingue.includes(original_language)) {
+
+        flags ='<img src="' + original_language + '.png">';
+        return flags;
+
+      } else {
+        return original_language;
+      }
+    };
+
+
+    function star(vote_average){
+
+      var vote = Math.floor(vote_average / 2);
+      var stars = "";
+
+        for (var s = 0; s <5; s++) {
+
+          if (s <= vote){
+            stars += '<i class="fas fa-star"></i>';
+         }else{
+            stars += '<i class="far fa-star"></i>';
+        }
+      }
+      return stars;
+    };
+
+
+      function posters(poster_path){
+
+        var img = "https://image.tmdb.org/t/p/w154" + poster_path;
+
+        if(poster_path === null){
+          poster = "No image";
+        }else{
+          poster = '<img src="' + img + '" class="poster">';
+        }
+        return poster;
+
+    };
+
+
+
+
 
 });
